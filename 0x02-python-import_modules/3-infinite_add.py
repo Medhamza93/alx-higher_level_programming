66073dd6cd63c1d56a36ae55df747ccac58cdbcd
Medhamza93 <medhamzalahnin@gmail.com>
2023-11-02 17:08:08 -0700

    rassna

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    total = 0
+    n = int(len(sys.argv))
+    for i in range(n - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
